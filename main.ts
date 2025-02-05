81577f6d73964e53e65a4bcad1de57d1d0f2d093
amarlop126 <amarlop126@g.educaand.es>
2025-02-05 12:36:53 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
 basic.forever(function () {
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    music.play(music.stringPlayable("C5 B A G E G E D ", 120), music.PlaybackMode.UntilDone)
+})
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == maqueen.readPatrol(maqueen.Patrol.PatrolRight)) {
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 80)
+        maqueen.motorStop(maqueen.Motors.M1)
+        basic.pause(500)
+    }
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-    basic.pause(1000)
-    maqueen.motorStop(maqueen.Motors.All)
-    basic.pause(1000)
 })
